6ad3363aa68bfe311e017641b52e2b3ef74a2405
Matthew Edmond <medmond95@gmail.com>
2016-08-11 11:42:44 -0400

    added token

--- app.js
+++ app.js
@@ -1,66 +1,70 @@
 "use strict";
- 
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const twilio = require('twilio');
 const app = express();
 const request = require('request');
 
-
 var port = process.env.PORT || 8080; // set our port
 
 
 // Run server to listen on port assigned by Heroku.
-const server = app.listen(port, () => {
-  console.log('listening on *:' + port);
-  console.log('Tokenb is :' + process.env.TOKEN);
-  console.log('URL is' + process.env.URL +'/rest/servicedeskapi/request');
-});
+const server = app.listen(port, () =  > {
+			console.log('listening on *:' + port);
+			console.log('Tokenb is :' + process.env.TOKEN);
+			console.log('URL is' + process.env.URL + '/rest/servicedeskapi/request');
+		});
 
+app.use(bodyParser.urlencoded({
+		extended : false
+	}));
 
-app.use(bodyParser.urlencoded({ extended: false } ));
- 
 // Set Express routes.
-app.post('/events', (req, res) => {
-  let to = req.body.To;
-  let fromNumber = req.body.From;
-  let callStatus = req.body.CallStatus;
-  let callSid = req.body.CallSid;
- 
- 
+app.post('/events', (req, res) =  > {
+		let to = req.body.To;
+		let fromNumber = req.body.From;
+		let callStatus = req.body.CallStatus;
+		let callSid = req.body.CallSid;
 
-	var options = { method: 'POST',
-		  url: process.env.URL,
-		  headers: 
-		   { authorization: 'Basic'+ process.env.TOKEN },
-		  body: 
-		   { serviceDeskId: '1',
-			 requestTypeId: '1',
-			 requestFieldValues: 
-			  { summary: 'Inbound Phone request from ' + fromNumber,
-				description: 'A request has been received via phone' } },
-		  json: true };
+		var options = {
+			method : 'POST',
+			url : process.env.URL,
+			headers : {
+				'postman-token' : 'b54b3484-c975-af14-6b0d-b04516542f9e',
+				'cache-control' : 'no-cache',
+				'content-type' : 'application/json',
+				authorization : 'Basic' + process.env.TOKEN
+			},
+			body : {
+				serviceDeskId : '1',
+				requestTypeId : '1',
+				requestFieldValues : {
+					summary : 'Inbound Phone request from ' + fromNumber,
+					description : 'A request has been received via phone'
+				}
+			},
+			json : true
+		};
 
 		request(options, function (error, response, body) {
-		  if (error) throw new Error(error);
-		console.log(body);
+			if (error)
+				throw new Error(error);
+			console.log(body);
 
 		});
 
+		console.log(to, fromNumber, callStatus, callSid);
+		res.send('Event received');
+	});
 
-
-console.log(to, fromNumber, callStatus, callSid);
-  res.send('Event received');
-});
-
-
-app.post('/voice', (req, res) => {
-  // Generate a TwiML response
-  let twiml = new twilio.TwimlResponse();
-  // Talk in a robot voice over the phone.
-  twiml.say('Call progress events are rad');
-  // Set the response type as XML.
-  res.header('Content-Type', 'text/xml');
-  // Send the TwiML as the response.
-  res.send(twiml.toString());
-});
+app.post('/voice', (req, res) =  > {
+		// Generate a TwiML response
+		let twiml = new twilio.TwimlResponse();
+		// Talk in a robot voice over the phone.
+		twiml.say('Call progress events are rad');
+		// Set the response type as XML.
+		res.header('Content-Type', 'text/xml');
+		// Send the TwiML as the response.
+		res.send(twiml.toString());
+	});
