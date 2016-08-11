3a4db98147fe8e2b3ecca082ad16bfaa53988b46
Matthew Edmond <medmond95@gmail.com>
2016-08-11 09:28:54 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Twilio Call Monitoring for JIRA
+
+
+To test my progress, try to deploy it using this button:
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
---
+++ app.js
@@ -0,0 +1,52 @@
+"use strict";
+ 
+const express = require('express');
+const bodyParser = require('body-parser');
+const twilio = require('twilio');
+const app = express();
+const request = require('request');
+
+
+var port = process.env.PORT || 8080; // set our port
+
+
+// Run server to listen on port assigned by Heroku.
+const server = app.listen(port, () => {
+  console.log('listening on *:' + port);
+});
+
+
+app.use(bodyParser.urlencoded({ extended: false } ));
+ 
+// Set Express routes.
+app.post('/events', (req, res) => {
+  let to = req.body.To;
+  let fromNumber = req.body.From;
+  let callStatus = req.body.CallStatus;
+  let callSid = req.body.CallSid;
+ 
+ 
+
+	var options = { method: 'POST',
+		  url: process.env.URL +'/rest/servicedeskapi/request',
+		  headers: 
+		   { 'authorization: 'Basic'+ port.env.TOKEN },
+		  body: 
+		   { serviceDeskId: '1',
+			 requestTypeId: '1',
+			 requestFieldValues: 
+			  { summary: 'Inbound Phone request from ' + fromNumber,
+				description: 'A request has been received via phone' } },
+		  json: true };
+
+		request(options, function (error, response, body) {
+		  if (error) throw new Error(error);
+		console.log(body);
+
+		});
+
+
+
+console.log(to, fromNumber, callStatus, callSid);
+  res.send('Event received');
+});
