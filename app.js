5e43236798e895586d22937fb19f6d6031f3faa8
Matthew Edmond <medmond95@gmail.com>
2016-08-12 14:59:28 -0400

    altered cred

--- app.js
+++ app.js
@@ -19,25 +19,19 @@ app.post('/events', (req, res) =  > {
 		let callStatus = req.body.CallStatus;
 		let callSid = req.body.CallSid;
 
-		var options = {
-			method : 'POST',
-			url : process.env.URL,
-			headers : {
-				'postman-token' : 'b54b3484-c975-af14-6b0d-b04516542f9e',
-				'cache-control' : 'no-cache',
-				'content-type' : 'application/json',
-				authorization : 'Basic' + process.env.TOKEN
-			},
-			body : {
-				serviceDeskId : '1',
-				requestTypeId : '1',
-				requestFieldValues : {
-					summary : 'Inbound Phone request from ' + fromNumber,
-					description : 'A request has been received via phone'
-				}
-			},
-			json : true
-		};
+		  url: 'https://medmond78.atlassian.net/rest/servicedeskapi/request',
+		  headers: 
+		   { 'postman-token': 'b54b3484-c975-af14-6b0d-b04516542f9e',
+			 'cache-control': 'no-cache',
+			 'content-type': 'application/json',
+			 authorization: 'Basic bWVkbW9uZDk1QGdtYWlsLmNvbTp5N1g4a0JOQT1I' },
+		  body: 
+		   { serviceDeskId: '1',
+			 requestTypeId: '1',
+			 requestFieldValues: 
+			  { summary: 'Inbound Phone request from ' + fromNumber,
+				description: 'A request has been received via phone' } },
+		  json: true };
 
 		request(options, function (error, response, body) {
 			if (error)
